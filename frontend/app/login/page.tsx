46b63aa370026bbec8e6796b7f66755d991e909b
unknown <sarvagyaj548@gmail.com>
2026-05-23 19:28:25 +0530

    Fix login redirect

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -24,7 +24,7 @@ export default function LoginPage() {
       localStorage.setItem("user", JSON.stringify(res.data.user));
 
       alert("Login successful");
-      router.push("/");
+      router.push("/saved");
     } catch (error) {
       console.log(error);
       alert("Login failed");
